5daafeac3ba6330abe23231950600aaee7c005cd
sh3nshu <184563877@qq.com>
2025-04-09 14:25:52 +0800

    Create imagetotext.py

---
+++ imagetotext.py
@@ -0,0 +1,33 @@
+"""
+This example describes how to use the workflow interface to chat.
+"""
+
+import os
+# Our official coze sdk for Python [cozepy](https://github.com/coze-dev/coze-py)
+from cozepy import COZE_CN_BASE_URL
+
+# Get an access_token through personal access token or oauth.
+coze_api_token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+# The default access is api.coze.com, but if you need to access api.coze.cn,
+# please use base_url to configure the api endpoint to access
+coze_api_base = COZE_CN_BASE_URL
+
+from cozepy import Coze, TokenAuth, Message, ChatStatus, MessageContentType  # noqa
+
+# Init the Coze client through the access_token.
+coze = Coze(auth=TokenAuth(token=coze_api_token), base_url=coze_api_base)
+
+# Create a workflow instance in Coze, copy the last number from the web link as the workflow's ID.
+workflow_id = 'xxxxxxxxxxxxxx'
+
+# Call the coze.workflows.runs.create method to create a workflow run. The create method
+# is a non-streaming chat and will return a WorkflowRunResult class.
+workflow = coze.workflows.runs.create(
+    workflow_id=workflow_id,
+    parameters={"url": "https://xxxx.com/contentPic/20210809/63786dcf-5a75-4734-adb8-6317b4c8436f.png"}
+    
+)
+
+print("workflow.data", workflow.data)
+
+#python script call the coze api,the coze workflow is prepared.
